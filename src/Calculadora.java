97bd4aec905a68d1230bf2d440cc6dfea1634b30
German <gromanoalvarez@alumno.unlam.edu.ar>
2022-05-10 01:01:30 -0300

    Consignas 5 y 6

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,7 +9,7 @@ public class Calculadora {
 		
 		Scanner teclado= new Scanner(System.in);
 		do{
-			System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+			System.out.println("Ingrese su operación /n 1 para restar /n 2 para sumar /n 3 para dividir /n 4 para multiplicar");
 					opcion = teclado.nextInt();
 		}while(opcion < 1 && opcion > 4);
 		
@@ -20,14 +20,31 @@ public class Calculadora {
 		b = teclado.nextInt();
 		
 		if(opcion.equals(1)){
-			resultado = a + b;
-		}else if(opcion.equals(2)){
 			resultado = a - b;
+
+		}else if(opcion.equals(2)){
+			resultado = a + b;
+
 		}else if(opcion.equals(3)){
-			resultado = a * b;
-		}else{
 			resultado = a / b;
+
+		}else{
+			resultado = a * b;
+
 		}
 		System.out.println("El resultado es " + resultado.toString());
 	}
+	
+	/*5. ¿Qué sucedió cuando se intento hacer el segundo push?
+	 * Git notifica que han realizado cambios en el repositorio remoto 
+	 * ¿Qué acciones hay que tomar antes de realizar el push?
+	 *  que debo hacer un git pull antes de pushear.
+	 *  ¿Qué estrategia propondrían para evitar esta situación?
+	 *  trabajar en diferentes ramas
+	 */
+	
+	/*6.En el repositorio local ejecutar el siguiente comando git checkout -b develop
+		¿Qué acción realiza el parámetro -b?
+	 * hace que se cree una nueva rama como si se llamara a git-branch
+	 */
 }
